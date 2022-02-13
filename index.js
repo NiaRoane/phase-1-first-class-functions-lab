b0458d9081137b01936025852d3f9dafd5d59a85
+ <+>
2022-02-12 22:59:57 -0800

    commit tree e62bdb0ca4dca269a935508caef7d28feb134e22

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    const firstTwo = [drivers[0] , drivers[1]]
+    return firstTwo
+}
+function returnLastTwoDrivers(drivers){
+    const lastTwo = [drivers[2] , drivers[3]]
+    return lastTwo
+}
+
+function selectingDrivers(firstTwo){
+    return returnFirstTwoDrivers
+}
